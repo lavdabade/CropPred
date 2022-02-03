c8f41b6e92c36c8bbd7f1f7f3d4a4c3e6ecaaa79
Lav Dabade <59640859+lavdabade@users.noreply.github.com>
2022-02-03 15:05:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 import os
 
-port = int(os.environ.get('PORT', 8000))
+port = int(os.environ.get('PORT', 33507))
 app = Flask(__name__)
 app.run(host='0.0.0.0', port=port, debug=True)
 
